0d52e2f5274a9c02ae03444e7ccd470a43a7b3a8
Nash Nziramasanga <nash@linktr.ee>
2025-09-04 23:29:54 +1000

    added fetchAllBreeds

--- DogBreedQuiz/DogAPI.swift
+++ DogBreedQuiz/DogAPI.swift
@@ -9,10 +9,35 @@ import Foundation
 
 struct DogAPI {
     static let randomImageURL = URL(string: "https://dog.ceo/api/breeds/image/random")!
+    static let allBreedsURL = URL(string: "https://dog.ceo/api/breeds/list/all")!
     
     struct DogImageResponse: Codable {
         let message: String
         let status: String
+    }
+    
+    struct AllBreedsResponse: Codable {
+        let message: [String: [String]]
+        let status: String
+    }
+    
+    static func fetchAllBreeds(completion: @escaping (Result<[String], Error>) -> Void) {
+        URLSession.shared.dataTask(with: allBreedsURL) { data, _, error in
+            if let error = error {
+                completion(.failure(error))
+                return
+            }
+            
+            guard let data = data,
+                  let decoded = try? JSONDecoder().decode(AllBreedsResponse.self, from: data) else {
+                completion(.failure(NSError(domain: "", code: -1)))
+                return
+            }
+            
+            // Get main breeds only (not sub-breeds)
+            let breeds = Array(decoded.message.keys).map { $0.capitalized }
+            completion(.success(breeds))
+        }.resume()
     }
     
     static func fetchRandomDogImage(completion: @escaping (Result<String, Error>) -> Void) {
